ac3e6cf79acb93738875c79cc82aa0021187644b
Saurav Sahu <mrsauravsahu@outlook.com>
2021-06-05 20:07:44 +0530

    fix coopy alias command handler

--- src/commands/copy-alias.ts
+++ src/commands/copy-alias.ts
@@ -1,19 +1,18 @@
 import * as vscode from 'vscode'
 import { commands, rootStoragePath } from '../constants'
+import { CustomProfile } from '../models/custom-profile'
 import { Command } from '../types'
 
 export const copyAliasCommand: Command = {
     name: commands.copyAlias,
-    handler: () => () => {
-        vscode.commands.registerCommand("customProfiles.copyAlias", (...args) => {
-            const { name } = args[0]
+    handler: () => (customProfile: CustomProfile) => {
+        const { name } = customProfile
 
-            const launchCommand = `code --user-data-dir='${rootStoragePath}/${name}/data' --extensions-dir='${rootStoragePath}/${name}/extensions' -n`
-            // TODO: Make platform independent
-            const aliasCommand = `alias ${name}-profile='${launchCommand}'`
+        const launchCommand = `code --user-data-dir='${rootStoragePath}/${name}/data' --extensions-dir='${rootStoragePath}/${name}/extensions' -n`
+        // TODO: Make platform independent
+        const aliasCommand = `alias ${name}-profile='${launchCommand}'`
 
-            vscode.env.clipboard.writeText(aliasCommand)
-            vscode.window.showInformationMessage('Copied alias command to the clipboard')
-        })
+        vscode.env.clipboard.writeText(aliasCommand)
+        vscode.window.showInformationMessage('Copied alias command to the clipboard')
     }
 }

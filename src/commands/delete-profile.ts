108fc552676a7be9198be4d995974ba10c4914f2
Saurav Sahu <mrsauravsahu@outlook.com>
2021-06-05 20:08:40 +0530

    fix delete profile command handler

--- src/commands/delete-profile.ts
+++ src/commands/delete-profile.ts
@@ -4,28 +4,27 @@ import * as path from 'path'
 
 import { commands, profiles, rootStoragePath, strings } from '../constants'
 import { Command } from '../types'
+import { CustomProfile } from '../models/custom-profile'
 
 export const deleteProfileCommand: Command = {
     name: commands.deleteProfile,
-    handler: ({ service, treeView }) => () => {
-        vscode.commands.registerCommand('customProfiles.delete', async (...args) => {
-            const { name } = args[0]
-            if (name === profiles.default) {
-                vscode.window.showErrorMessage('Cannot delete the default profile')
-                return
+    handler: ({ service, treeView }) => async (customProfile: CustomProfile) => {
+        const { name } = customProfile
+        if (name === profiles.default) {
+            vscode.window.showErrorMessage('Cannot delete the default profile')
+            return
+        }
+
+        const confirmation = await vscode.window.showInformationMessage('Are you sure you want to delete this profile?', 'Yes', 'Cancel')
+
+        if (confirmation === 'Yes') {
+            const profilePath = path.join(rootStoragePath, name)
+            child_process.exec(`rm -r ${profilePath}`)
+            vscode.window.showInformationMessage(`Successfully deleted custom profile: '${name}'`)
+
+            if (service.getAll().length === 0) {
+                treeView.message = strings.noProfiles
             }
-
-            const confirmation = await vscode.window.showInformationMessage('Are you sure you want to delete this profile?', 'Yes', 'Cancel')
-
-            if (confirmation === 'Yes') {
-                const profilePath = path.join(rootStoragePath, name)
-                child_process.exec(`rm -r ${profilePath}`)
-                vscode.window.showInformationMessage(`Successfully deleted custom profile: '${name}'`)
-
-                if (service.getAll().length === 0) {
-                    treeView.message = strings.noProfiles
-                }
-            }
-        })
+        }
     }
 }

862b0957e8a4aa57a3ca3779077a9f96fa78a12c
Saurav Sahu <mrsauravsahu@outlook.com>
2021-06-06 18:23:32 +0530

    fix branch name

--- src/services/featured-profile.service.ts
+++ src/services/featured-profile.service.ts
@@ -2,12 +2,12 @@ import * as path from 'path'
 import * as vscode from 'vscode'
 import axios from 'axios'
 import * as constants from '../constants'
-import {CustomProfile} from '../models/custom-profile'
+import { CustomProfile } from '../models/custom-profile'
 
 export class FeaturedProfileService {
   async getProfileDetails(profile: string): Promise<string> {
-    const url = `https://raw.githubusercontent.com/mrsauravsahu/vscode-manager/feat/featured-profiles/featured/${profile}`
-    const featuredProfileListResponse = await axios({url, responseType: 'text'})
+    const url = `https://raw.githubusercontent.com/mrsauravsahu/vscode-manager/cool/featured/${profile}`
+    const featuredProfileListResponse = await axios({ url, responseType: 'text' })
     const profileDetails = (featuredProfileListResponse.data)
 
     if (typeof profileDetails === 'object') {
@@ -18,12 +18,12 @@ export class FeaturedProfileService {
   }
 
   async getAll(): Promise<CustomProfile[]> {
-    const branchRef = 'feat/featured-profiles'
+    const branchRef = 'cool'
     const url = `${constants.featuredProfileUrl}?ref=${branchRef}`
     const featuredProfileListResponse = await axios(url)
 
     const profiles = (featuredProfileListResponse.data as any[])
-      .map((item: {name: string}) => {
+      .map((item: { name: string }) => {
         const profileName = (item.name).replace('.json', '')
         const profile = new CustomProfile(
           `${constants.app}:models.customProfile.${profileName}`,

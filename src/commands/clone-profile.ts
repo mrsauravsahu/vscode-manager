adf770bb12893ad27d12781d7fe64169a52d16c7
Saurav Sahu <mrsauravsahu@outlook.com>
2021-09-27 01:01:56 +0530

    fix: run a lint fix

--- src/commands/clone-profile.ts
+++ src/commands/clone-profile.ts
@@ -1,43 +1,40 @@
-import * as child_process from 'child-process-promise'
-import * as vscode from 'vscode'
 import * as fs from 'fs'
 import * as path from 'path'
+import * as child_process from 'child-process-promise'
+import * as vscode from 'vscode'
 
-import { commands, rootStoragePath } from '../constants'
-import { CustomProfile } from '../models/custom-profile'
-import { Command } from '../types'
-
-// TODO: Not fully implemented
+import {commands, rootStoragePath} from '../constants'
+import {CustomProfile} from '../models/custom-profile'
+import {Command} from '../types'
 
 export const cloneProfileCommand: Command = {
   name: commands.cloneProfile,
-  handler: ({ provider, services: [_, __, commandGeneratorService] }) => async (customProfile: CustomProfile) => vscode.window.withProgress({
+  handler: ({provider, services: [_, __, commandGeneratorService]}) => async (customProfile: CustomProfile) => vscode.window.withProgress({
     location: vscode.ProgressLocation.Notification,
     title: 'Clone',
-    cancellable: false
+    cancellable: false,
   }, async progress => {
-    const { name } = customProfile
-    const currentDate = new Date();
+    const {name} = customProfile
+    const currentDate = new Date()
     const cloneProfileName = `${name}-${currentDate.getFullYear()}${currentDate.getMonth().toString().padStart(2, '0')}${currentDate.getDate().toString().padStart(2, '0')}-${currentDate.getTime()}`
-
 
     const originalProfilePath = path.join(rootStoragePath, name)
 
     const clonedProfilePath = path.join(rootStoragePath, cloneProfileName)
 
-    const createCloneProfileCommand = commandGeneratorService.generateCommand('mkdir', `${clonedProfilePath}/data/User`, { Linux: '-p', Darwin: '-p', Windows_NT: undefined })
+    const createCloneProfileCommand = commandGeneratorService.generateCommand('mkdir', `${clonedProfilePath}/data/User`, {Linux: '-p', Darwin: '-p', Windows_NT: undefined})
 
-    progress.report({ increment: 5, message: 'Creating profile folder...' });
-    await child_process.exec(createCloneProfileCommand.command, { shell: createCloneProfileCommand.shell })
+    progress.report({increment: 5, message: 'Creating profile folder...'})
+    await child_process.exec(createCloneProfileCommand.command, {shell: createCloneProfileCommand.shell})
 
     await fs.promises.copyFile(path.join(originalProfilePath, 'data', 'User', 'settings.json'),
       path.join(clonedProfilePath, 'data', 'User', 'settings.json'))
 
     // Get extensions
-    const { command: getExtensionsCommand, shell } = commandGeneratorService.generateCommand('code', `--user-data-dir '${path.join(originalProfilePath, 'data')}' --extensions-dir '${path.join(originalProfilePath, 'extensions')}' --list-extensions`)
+    const {command: getExtensionsCommand, shell} = commandGeneratorService.generateCommand('code', `--user-data-dir '${path.join(originalProfilePath, 'data')}' --extensions-dir '${path.join(originalProfilePath, 'extensions')}' --list-extensions`)
 
-    progress.report({ increment: 10, message: 'Retrieving extensions from old profile...' });
-    const getExtensionsCommandOutput = await child_process.exec(getExtensionsCommand, { shell })
+    progress.report({increment: 10, message: 'Retrieving extensions from old profile...'})
+    const getExtensionsCommandOutput = await child_process.exec(getExtensionsCommand, {shell})
     const extensions = getExtensionsCommandOutput
       .stdout
       .toString()
@@ -45,24 +42,24 @@ export const cloneProfileCommand: Command = {
       .split(/[\n\r]/)
       .filter(p => p.trim() !== '')
 
-    let finishedExtensionCount = 1;
+    let finishedExtensionCount = 1
 
     const selectedExtensions = await vscode.window.showQuickPick(extensions, {
       title: 'Please select the extensions you want to clone',
       canPickMany: true,
-    }) || []
+    }) ?? []
 
-    const installExtensionPromises = selectedExtensions.map(async (extension) => new Promise<void>(async (resolve) => {
-      const { command: extensionInstallCommand, shell } = commandGeneratorService.generateCommand('code', `--user-data-dir '${path.join(clonedProfilePath, 'data')}' --extensions-dir '${path.join(clonedProfilePath, 'extensions')}' --install-extension ${extension}`)
+    const installExtensionPromises = selectedExtensions.map(async extension => new Promise<void>(async resolve => {
+      const {command: extensionInstallCommand, shell} = commandGeneratorService.generateCommand('code', `--user-data-dir '${path.join(clonedProfilePath, 'data')}' --extensions-dir '${path.join(clonedProfilePath, 'extensions')}' --install-extension ${extension}`)
 
       progress.report({
         increment: 50,
-        message: `Installing extension '${extension}' (${finishedExtensionCount}/${selectedExtensions.length}) ...`
+        message: `Installing extension '${extension}' (${finishedExtensionCount}/${selectedExtensions.length}) ...`,
       })
 
-      await child_process.exec(extensionInstallCommand, { shell })
+      await child_process.exec(extensionInstallCommand, {shell})
 
-      finishedExtensionCount += 1;
+      finishedExtensionCount += 1
       resolve()
     }))
 
@@ -70,7 +67,7 @@ export const cloneProfileCommand: Command = {
     provider.refresh()
     progress.report({
       increment: 100,
-      message: `Finished installing extensions.`
+      message: 'Finished installing extensions.',
     })
 
     await vscode.window.showInformationMessage(`Cloned profile '${name}' to '${cloneProfileName}'.`)

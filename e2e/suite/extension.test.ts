efd825ae0c795a162f8c2c73469d093119283220
Saurav Sahu <mrsauravsahu@outlook.com>
2022-07-10 13:11:48 +0530

    chore: add a sample jest test

---
+++ e2e/suite/extension.test.ts
@@ -0,0 +1,5 @@
+describe('test', () => {
+  test('bruh', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
---
+++ e2e/suite/index.ts
@@ -0,0 +1,15 @@
+import * as jest from 'jest'
+
+export async function run(): Promise<void> {
+  const jestRunResult = await jest.runCLI(({
+    ci: true,
+    color: true,
+    roots: ['./e2e'],
+  } as any), ['e2e'])
+
+  if (jestRunResult.results.success) {
+    console.log('Tests completed')
+  } else {
+    console.error('Tests failed')
+  }
+}
